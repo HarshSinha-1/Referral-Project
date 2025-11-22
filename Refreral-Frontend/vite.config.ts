408cbba4396980705727937acca2d5d8da6110ff
HarshSinha-1 <harshsinha969@gmail.com>
2025-11-22 13:46:01 +0530

    error2

--- Refreral-Frontend/vite.config.ts
+++ Refreral-Frontend/vite.config.ts
@@ -2,18 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   build: {
-    outDir: 'dist',  // avoid any source maps that use eval
+    outDir: 'dist',
   },
-  
-  // Render expects dist
-  
+
   server: {
-    // optional, but you can disable HMR if needed
+    host: true,
     hmr: {
       overlay: false
     }
